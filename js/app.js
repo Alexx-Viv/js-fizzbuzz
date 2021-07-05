15705908d6acfd46fe31ffd50645f68482c0f1d4
Alexx-Viv <mario.alex.vivsenco@gmail.com>
2021-07-05 16:59:44 +0200

    Fix utility

--- js/app.js
+++ js/app.js
@@ -3,14 +3,14 @@ var numbers = [];
 
 for(var i = 1; i <= 100; i++){
     utility = i;
-    if (utility % 3 == 0){
-        if (utility % 5 == 0){
+    if (i % 3 == 0){
+        if (i % 5 == 0){
             utility = "fizz buzz";
         } else {
             utility = "fizz";
         }
     }
-    else if( utility % 5 == 0){
+    else if( i % 5 == 0){
         utility = "buzz";
     }
     numbers.push(utility);
