e1f2f9fe9b00b6da6983e65fb611cf36e319d31b
Alexx-Viv <mario.alex.vivsenco@gmail.com>
2021-07-05 14:52:47 +0200

    Complete

---
+++ js/app.js
@@ -0,0 +1,19 @@
+var utility;
+var numbers = [];
+
+for(var i = 0; i <= 100; i++){
+    utility = i;
+    if (utility % 3 == 0){
+        if (utility % 5 == 0){
+            utility = "fizz buzz";
+        } else {
+            utility = "fizz";
+        }
+    }
+    else if( utility % 5 == 0){
+        utility = "buzz";
+    }
+    numbers.push(utility);
+}
+
+alert(numbers);
